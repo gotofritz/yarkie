05e4d08723a7799f04539d006a53adafebe30cc2
gotofritz <gotofritz@users.noreply.github.com>
2023-10-21 13:33:23 +0200

    create quick and dirty ui code to visualise channels list

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const DEFAULT_CHANNELS: string[] = [
+  "hey",
+  "you",
+  "there",
+  JSON.stringify({ a: 1, b: 2 }, null, 2),
+];
+
 function App() {
-  return <div className="App">No channels found</div>;
+  const serverUrl = "https://localhost:1234";
+  const [channels, setChannels] = useState(DEFAULT_CHANNELS);
+  const [pageErrors, setPageErrors] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    setChannels([]);
+    fetch(serverUrl)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (!ignore) {
+            setChannels(result);
+          }
+        },
+        (error) => {
+          setChannels(DEFAULT_CHANNELS);
+          setPageErrors(error.message);
+        }
+      );
+    return () => {
+      ignore = true;
+    };
+  }, [serverUrl]);
+
+  const channelList = (channels: string[] = DEFAULT_CHANNELS) => {
+    return (
+      <>
+        {channels.map((data) => {
+          return <div className="channel">{data as string}</div>;
+        })}
+      </>
+    );
+  };
+  return (
+    <div className="App">
+      <h1>Yarkie</h1>
+      {pageErrors && <div className="error">Error: {pageErrors}</div>}
+      {channels.length == 0 && <div className="empty">No channels found</div>}
+      {channels.length > 0 && channelList(channels)}
+    </div>
+  );
 }
 
 export default App;
