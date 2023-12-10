9d47667730fb9e551336311c468f2cceb9940473
gotofritz <gotofritz@users.noreply.github.com>
2023-12-10 23:54:02 +0100

    add mypy

--- tools/tools/repositories.py
+++ tools/tools/repositories.py
@@ -1,9 +1,9 @@
 import json
 from pathlib import Path
-from typing import Any, TypeAlias
+from typing import cast, Any, TypeAlias
 
 from sqlite_utils import Database
-from sqlite_utils.db import NotFoundError
+from sqlite_utils.db import NotFoundError, Table
 
 DB_PATH = Path(__file__).parent.parent.parent / "data/yarkie.db"
 
@@ -12,7 +12,7 @@ DBData: TypeAlias = dict[str, list[dict[str, Any]]]
 
 class DataRepository:
     def __init__(self, data: DBData | None = None):
-        self.dbpath: str | None = None
+        self.dbpath: Path | None = None
         self.db: Database | None = None
         if isinstance(data, str):
             parsed = json.loads(data)
@@ -23,15 +23,23 @@ class DataRepository:
             self.dbpath = DB_PATH
             self.db = Database(self.dbpath)
 
-    def _load_data(self, init_data: DBData) -> bool:
+    def _load_data(self, init_data: DBData) -> None:
         """Load data into the in memory db. Used for testing."""
-        for table, data in init_data.items():
-            self.db[table].insert_all(data, pk="id")
+        if self.db is None:
+            return
+        for table_name, data in init_data.items():
+            # this nonsense is needed to keep mypy happy
+            table = cast(Table, self.db[table_name])
+            table.insert_all(data, pk="id")
 
     def playlist_exists(self, id: str) -> bool:
         """Look up a playlist in the db."""
+        if self.db is None:
+            return False
         try:
-            self.db["playlists"].get(id)
+            # this nonsense is needed to keep mypy happy
+            table = cast(Table, self.db["playlists"])
+            table.get(id)
             return True
         except NotFoundError:
             return False
