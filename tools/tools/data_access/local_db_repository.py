6077064336368ec30bc04d4f9138dfb09a17d931
gotofritz <gotofritz@users.noreply.github.com>
2023-12-14 00:45:01 +0100

    remove more obsolete methods

--- tools/tools/data_access/local_db_repository.py
+++ tools/tools/data_access/local_db_repository.py
@@ -4,10 +4,10 @@
 
 import json
 from pathlib import Path
-from typing import Any, Generator, TypeAlias, cast
+from typing import Any, TypeAlias, cast
 
 from sqlite_utils import Database
-from sqlite_utils.db import NotFoundError, Table
+from sqlite_utils.db import Table
 from tools.models.models import Playlist, Video, YoutubeObj
 from tools.models.models import last_updated_factory
 from tools.settings import DB_PATH
@@ -18,14 +18,7 @@ DBData: TypeAlias = dict[str, list[dict[str, Any]]]
 
 class LocalDBRepository:
     """
-    Manage the database connection and provides utility methods.
-
-    The database can be either a real SQLite database or an in-memory
-    one for testing purposes.
-
-    Methods:
-    --------
-    playlist_exists: Returns True if a playlist exists in the database.
+    Comment
     """
 
     def __init__(self, data: DBData | None = None):
@@ -61,35 +54,6 @@ class LocalDBRepository:
             # Type casting to keep mypy happy
             table = cast(Table, self.db[table_name])
             table.insert_all(data, pk="id")
-
-    def playlist_exists(self, key: str) -> bool:
-        """Check if a playlist exists in the database."""
-        try:
-            # Type casting to keep mypy happy
-            table = cast(Table, self.db["playlists"])
-            table.get(key)
-            return True
-        except NotFoundError:
-            return False
-
-    def get_playlist_videos(self, playlist_key: str) -> Generator[dict, None, None]:
-        # Type casting to keep mypy happy
-        table = cast(Table, self.db["videos"])
-        return table.rows_where("playlist_id=?", [playlist_key])
-
-    def get_playlist_videos_ids_with_thumbnail(self, playlist_key: str) -> list[str]:
-        return [
-            row["id"]
-            for row in list(self.get_playlist_videos(playlist_key=playlist_key))
-            if row["thumbnail"] and not row["thumbnail"].startswith("http")
-        ]
-
-    def get_playlist_videos_ids_with_videos(self, playlist_key: str) -> list[str]:
-        return [
-            row["id"]
-            for row in list(self.get_playlist_videos(playlist_key=playlist_key))
-            if row["video_file"]
-        ]
 
     def update_playlists(self, all_records: list[YoutubeObj]):
         """Comment."""
